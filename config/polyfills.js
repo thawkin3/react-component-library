b8e99d4724570366bf0dc35238bf334a72c8f4a2
Tyler Hawkins <thawkins@youniqueproducts.com>
2019-02-02 15:22:09 -0700

    added flow-typed directory; set package to not private

--- config/polyfills.js
+++ config/polyfills.js
@@ -37,10 +37,8 @@ if (typeof window !== 'undefined') {
     window.matchMedia = window.matchMedia || function () {
         return {
             matches: false,
-            addListener: function () {
-            },
-            removeListener: function () {
-            }
+            addListener: function () {},
+            removeListener: function () {},
         };
     };
 }
